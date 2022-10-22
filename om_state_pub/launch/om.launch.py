309d3b18a57b35b5f926de6b103f090e86b90c2b
jrajaram20 <jrajaram20@gmail.com>
2022-10-22 18:08:30 +0800

    ipchange

--- om_state_pub/launch/om.launch.py
+++ om_state_pub/launch/om.launch.py
@@ -10,7 +10,7 @@ def generate_launch_description():
         #name = 'action_server',
         #output='screen',
         parameters=[{
-            'ip_address': "192.168.250.175",
+            'ip_address': "192.168.56.10"
             'port': 7171,
             'arcl_passwd': "omron"
         }]
@@ -21,7 +21,7 @@ def generate_launch_description():
         #name = 'action_server',
         output='screen',
         parameters=[{
-            'ip_address': "192.168.250.176",
+            'ip_address': "192.168.56.10",
             'port': 7171,
             'arcl_passwd': "omron"
         }]
@@ -32,7 +32,7 @@ def generate_launch_description():
         #name='amr1_pub',
         #output='screen',
         parameters=[{
-            'ip_address': "192.168.250.175",
+            'ip_address': "192.168.56.10",
             'port': 7171,
             'arcl_passwd': "omron",
             'amr_name': "amr1"
